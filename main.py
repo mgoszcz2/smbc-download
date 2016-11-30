41c78b4b212dcf8732b2b220ee9c16f001e805e3
Maciej Goszczycki <mgoszcz2@gmail.com>
2016-11-30 23:34:23 +0000

    Added license to python

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# smbc-download - Download top 100 comics from smbc
+# Copyright (C) 2016 Maciej Goszczycki
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import requests
 from bs4 import BeautifulSoup
 import os
